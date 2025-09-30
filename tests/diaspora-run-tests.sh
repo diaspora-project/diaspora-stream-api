fd74f436518e415bc1d350d362114c725ee66656
Matthieu Dorier <mdorier@anl.gov>
2025-09-30 15:47:57 +0100

    updated script that runs tests so that it returns an error when failing

--- tests/diaspora-run-tests.sh
+++ tests/diaspora-run-tests.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 BEFORE_COMMAND=""
 AFTER_COMMAND=""
+RET=0
 
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -40,13 +41,27 @@ for test_file in ${SCRIPT_DIR}/Diaspora*Test ; do
     echo "Running test file ${test_file}"
     if [ -n "$BEFORE_COMMAND" ]; then
         eval "$BEFORE_COMMAND"
+        if [ "$?" -ne 0 ]; then
+            RET=1
+        fi
     fi
     ${test_file}
+    if [ "$?" -ne 0 ]; then
+        RET=1
+    fi
     if [ -n "$AFTER_COMMAND" ]; then
         eval "$AFTER_COMMAND"
+        if [ "$?" -ne 0 ]; then
+            RET=1
+        fi
     fi
 done
 
 # Python tests
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SCRIPT_DIR/../lib \
     python -m unittest discover diaspora_stream
+if [ "$?" -ne 0 ]; then
+    RET=1
+fi
+
+exit $RET
